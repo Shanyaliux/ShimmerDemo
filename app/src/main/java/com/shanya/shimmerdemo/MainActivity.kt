e4a34fb2d0122c3644ee2547c57efd6a4b0283ab
Shanyaliux <445502929@qq.com>
2020-03-23 09:45:48 +0800

    ok

--- app/src/main/java/com/shanya/shimmerdemo/MainActivity.kt
+++ app/src/main/java/com/shanya/shimmerdemo/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.shanya.shimmerdemo
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.Request
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        shimmerLayout.apply {
+            setShimmerColor(0x55FFFFFF)
+            setShimmerAngle(0)
+            startShimmerAnimation()
+        }
+
+        Glide.with(this)
+            .load("https://github.com/Shanyaliux/ShimmerDemo/raw/master/image/timg.jpg")
+            .placeholder(R.drawable.ic_insert_photo_gray_24dp)
+            .listener(object :RequestListener<Drawable>{
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false.also { shimmerLayout?.stopShimmerAnimation() }
+                }
+
+            })
+            .into(imageView)
     }
 }
